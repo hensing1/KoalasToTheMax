fe6a1da1c3be542e902fa11b5a07e4ba5fe69d77
Henning Lehmann <34438247+henry-dv@users.noreply.github.com>
2019-03-15 15:42:13 +0100

    stuff stuff

--- KoalasToTheMax/Program.cs
+++ KoalasToTheMax/Program.cs
@@ -13,24 +13,121 @@ namespace KoalasToTheMax
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("type \"go\" to start");
+            Console.WriteLine("\nWelcome to the koalastothemax.com auto-solver. \n\"help\" for commands");
             while (true)
             {
-                if (Console.ReadLine() == "go")
+                args = Console.ReadLine().ToLower().Split(new char[] { ' ' }, StringSplitOptions.RemoveEmptyEntries);
+                if (args[0] == "run")
                 {
-                    new Program().Default();
-                    break;
+                    if (args.Length > 1)
+                    {
+                        if (args.Contains("-d"))
+                        {
+                            if (args.Contains("-t") || args.Contains("-c"))
+                                Console.WriteLine("Too many arguments.");
+                            else
+                                new Program().Run();
+                        }
+                        else if (args.Contains("-c"))
+                        {
+                            try
+                            {
+                                new Program().Run(int.Parse(args[2]), int.Parse(args[3]));
+                            }
+                            catch (IndexOutOfRangeException ioore)
+                            {
+                                Console.WriteLine("Insufficient arguments");
+                                Console.WriteLine(ioore.StackTrace);
+                            }
+                            catch (FormatException re)
+                            {
+                                Console.WriteLine("Invalid format");
+                                Console.WriteLine(re.StackTrace);
+                            }
+                        }
+                        else if (args.Contains("-t"))
+                        {
+                            //Console.WriteLine("Please click the top left corner as precisely as possible.");
+
+                            Console.WriteLine("Uh, no. That's way more complicated than I anticipated it was going to be. Try sth else.");
+
+                            //UserActivityHook actHook = new UserActivityHook();
+                        }
+                        if (args.Contains("-s"))
+                        {
+
+                        }
+                    }
+                    else
+                    {
+                        new Program().Run();
+                    }
+                }
+                else if (args[0] == "track")
+                {
+                    new Program().Track();
+                }
+                else if (args[0] == "beep")
+                {
+                    Console.Beep();
+                }
+                else if (args[0] == "help")
+                {
+                    Console.WriteLine(String.Format("\n{0, -20} {1}\n", "Command", "Description"));
+                    Console.WriteLine(String.Format("{0, -20} {1}", "run", "Runs application with specified settings (esc to cancel)"));
+                    Console.WriteLine(String.Format("{0, -20} {1}", "  -d", "Run with default settings (FullHD screen)"));
+                    Console.WriteLine(String.Format("{0, -20} {1}", "  -c coordinates", "Run with custom settings (space delimited coordinates of top left corner)"));
+                    Console.WriteLine(String.Format("{0, -10} {1}", "  -t", "Run after clicking the corners of the image"));
+                    Console.WriteLine(String.Format("{0, -10} {1}", "  -s step line", "Specify the amount of milliseconds to wait after each step (std: 4) and line (std: 50)"));
+                    Console.WriteLine(String.Format("{0, -20} {1}", "track", "Output pointer position (any key to cancel)"));
+                    Console.WriteLine(String.Format("{0, -20} {1}", "beep", "beep"));
+                    Console.WriteLine(String.Format("{0, -20} {1}", "help", "This."));
+                    Console.WriteLine(String.Format("{0, -20} {1}\n", "exit", "Kills the application (you monster)"));
+                }
+                else if (args[0] == "exit")
+                {
+                    return;
                 }
                 else
                 {
                     Console.WriteLine("unknown command");
                 }
             }
+        }
 
+        void Run(int startX = 673, int startY = 318, int width = 512)
+        {
+            for (int i = 0; i < 7; i++)
+            {
+                for (int j = 0; j < Math.Pow(2, i); j++)
+                {
+                    int y = startY + width / (int)Math.Pow(2, i + 1) + width * j / (int)Math.Pow(2, i);
+                    Cursor.Position = new Point(startX, y);
+
+                    Thread.Sleep(50);
+
+                    for (int x = startX; x < startX + width; x += 4)
+                    {
+                        Cursor.Position = new Point(x, y);
+                        Thread.Sleep(1);
+                    }
+                    Thread.Sleep(50);
+
+                    if (Console.KeyAvailable && Console.ReadKey(true).Key == ConsoleKey.Escape)
+                    {
+                        Console.WriteLine("Program stopped (terminated by user)");
+                        return;
+                    }
+                }
+            }
+        }
+
+        void Track()
+        {
             int x = 0;
             int y = 0;
 
-            while(true)
+            while (!Console.KeyAvailable)
             {
                 if (Cursor.Position.X != x || Cursor.Position.Y != y)
                 {
@@ -38,32 +135,6 @@ namespace KoalasToTheMax
                     y = Cursor.Position.Y;
 
                     Console.WriteLine($"x: {x}, y: {y}");
-                }
-            }
-        }
-
-        void Default()
-        {
-            int startX = 673;
-            int startY = 318;
-            int endX = 1184;
-            int endY = 829;
-
-            for (int i = 0; i < 7; i++)
-            {
-                for (int j = 0; j < Math.Pow(2, i); j++)
-                {
-                    int y = startY + (endY - startY) * 1 / (int)Math.Pow(2, i + 1) + (endY - startY) * j / (int)Math.Pow(2, i);
-                    Cursor.Position = new Point(startX, y);
-
-                    Thread.Sleep(50);
-
-                    for (int x = startX; x < endX; x += 4)
-                    {
-                        Cursor.Position = new Point(x, y);
-                        Thread.Sleep(1);
-                    }
-                    Thread.Sleep(50);
                 }
             }
         }
