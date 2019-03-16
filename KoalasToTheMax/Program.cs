2d21fdce54158694d34a34ec745e05e6588734d9
Jens Lehmann <jenshenning.lehmann@rfh-campus.de>
2019-03-16 01:28:34 +0100

    here have a pause

--- KoalasToTheMax/Program.cs
+++ KoalasToTheMax/Program.cs
@@ -74,11 +74,11 @@ namespace KoalasToTheMax
                 else if (args[0] == "help")
                 {
                     Console.WriteLine(String.Format("\n{0, -20} {1}\n", "Command", "Description"));
-                    Console.WriteLine(String.Format("{0, -20} {1}", "run", "Runs application with specified settings (esc to cancel)"));
+                    Console.WriteLine(String.Format("{0, -20} {1}", "run", "Runs application with specified settings (esc to cancel, p to pause)"));
                     Console.WriteLine(String.Format("{0, -20} {1}", "  -d", "Run with default settings (FullHD screen)"));
                     Console.WriteLine(String.Format("{0, -20} {1}", "  -c coordinates", "Run with custom settings (space delimited coordinates of top left corner)"));
                     Console.WriteLine(String.Format("{0, -10} {1}", "  -t", "Run after clicking the corners of the image"));
-                    Console.WriteLine(String.Format("{0, -10} {1}", "  -s step line", "Specify the amount of milliseconds to wait after each step (std: 4) and line (std: 50)"));
+                    //Console.WriteLine(String.Format("{0, -10} {1}", "  -s step line", "Specify the amount of milliseconds to wait after each step (std: 4) and line (std: 50)"));
                     Console.WriteLine(String.Format("{0, -20} {1}", "track", "Output pointer position (any key to cancel)"));
                     Console.WriteLine(String.Format("{0, -20} {1}", "beep", "beep"));
                     Console.WriteLine(String.Format("{0, -20} {1}", "help", "This."));
@@ -97,6 +97,7 @@ namespace KoalasToTheMax
 
         void Run(int startX = 673, int startY = 318, int width = 512)
         {
+            Console.WriteLine("Running...");
             for (int i = 0; i < 7; i++)
             {
                 for (int j = 0; j < Math.Pow(2, i); j++)
@@ -109,17 +110,33 @@ namespace KoalasToTheMax
                     for (int x = startX; x < startX + width; x += 4)
                     {
                         Cursor.Position = new Point(x, y);
-                        Thread.Sleep(1);
+                        Thread.Sleep(2);
+                        if (Console.KeyAvailable)
+                        {
+                            if (Console.ReadKey(true).Key == ConsoleKey.P)
+                            {
+                                Console.WriteLine("Paused. p to continue");
+                                while (Console.KeyAvailable) { }
+                                while (true)
+                                {
+                                    if (Console.KeyAvailable && (Console.ReadKey(true).Key == ConsoleKey.P || Console.ReadKey(true).Key == ConsoleKey.Escape))
+                                    {
+                                        break;
+                                    }
+                                }
+                            }
+                            if (Console.ReadKey(true).Key == ConsoleKey.Escape)
+                            {
+                                Console.WriteLine("Program stopped (terminated by user)");
+                                return;
+                            }
+                            Console.WriteLine("Continuing...");
+                        }
                     }
-                    Thread.Sleep(50);
-
-                    if (Console.KeyAvailable && Console.ReadKey(true).Key == ConsoleKey.Escape)
-                    {
-                        Console.WriteLine("Program stopped (terminated by user)");
-                        return;
-                    }
+                    Thread.Sleep(70);
                 }
             }
+            Console.WriteLine("Finished!");
         }
 
         void Track()
